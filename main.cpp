2492fd6cb3865a29f9dfffd03ce8f3967f0b862d
250104 <240104@newuu.uz>
2025-09-08 14:10:00 +0500

    12334

---
+++ main.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+using namespace std;
+//
+// int main(){
+//
+//     int intNumber = 24 ;
+//     float floatNumber = 3.14 ;
+//     double doubleNumber = 45.1234 ;
+//     char charNumber = 'A' ;
+//     cout <<"Value of integer number is: " << intNumber <<"." << " Size of it is : " << sizeof(intNumber)<<endl;
+//     cout <<" Value of integer number is: " << floatNumber <<"."<< " Size of it is : " << sizeof(floatNumber) << endl;
+//     cout <<"Value of integer number is: " << doubleNumber <<"."<< " Size of it is : " << sizeof(doubleNumber) << endl;
+//     cout <<"Value of integer number is: " << charNumber <<"." << " Size of it is : " << sizeof(charNumber) << endl;
+//
+//     return 0;
+//
+// }
+
+// Problem #2
+// int main() {
+//     string name = "Alijon Aliev";
+//     int id = 23342;
+//     string song = "Queen - The Show Must Go On";
+//     string movie = "Titanic";
+//     cout << name << endl;
+//     cout << id << endl;
+//     cout << song << endl;
+//     cout << movie << endl;
+//     return 0;
+// }
+
+// Problem #3
+// int main() {
+//     cout << "\"I'm standing\" on the edge of some crazy clif."
+//             "\n What I have to do, I have to catch everybody\\"
+//             "\n If they start to go over the cliff-I mean\\"
+//             "\n \\if they're running and they don't look"
+//             "\n  where they're going I have to come out "
+//             "\n from somewhere and \t catch them. ";
+//     return 0;
+// }
+
+// Problem #4
+// int main() {
+//     int a = 189;
+//     char b ='B';
+//     int decValue = b;
+//     float num1 = a/3;
+//     float num2 = (a+b)/5;
+//     char c = b+a/10-1;
+//     cout << a <<"\t" << b <<"\t"<< c<<"\t" << num1 <<"\t"<< num2 <<"\t"<< endl;
+//     return 0;
+// }
+
+// Problem #5
+// int main() {
+//     int a = 12;
+//     int b = 38;
+//    int  sum = a + b;
+//     int product = a * b;
+//     cout <<"Sum = " << sum << endl;
+//     cout <<"Product = " << product << endl;
+//     return 0;
+//
+// }
+
+// Problem #6
+// int main() {
+//     int a = 38;
+//     int b = 73;
+//    int  sum = a + b;
+//     cout <<"Sum: "<< a <<" + " <<b<<" = "<< sum << endl;
+//     return 0;
+// }
+// Problem #7
+int main() {
+    double x, y;
+    cout << "Enter x value:";
+    cin >> x;
+    cout << "Enter y value:";
+    cin >> y;
+    int a = 3;
+    int b = 4;
+    float
+    t = (a(x*x+a))/((y/b)+b);
+     cout << t << endl;
+    return 0;
+}
